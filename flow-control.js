2c7692512cd5f5322d9ad95d92de39ca669963a1
Joanna Lin <lin.joanna.y@gmail.com>
2020-02-11 03:17:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,8 +2,7 @@ function basicTeenager(age) {
   if (age > 12 && age < 20) {
     return "You are a teenager!"
   }
-  return "You are not a teenager"
-  return undefined
+  
 }
 
 function teenager(age) {
