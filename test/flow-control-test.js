8251066588c6aa5e760d8d494cf0ed827b28eda0
Joanna Lin <lin.joanna.y@gmail.com>
2020-02-11 02:51:01 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -21,16 +21,15 @@ describe('flow-control', () => {
     })
   })
 
-  describe('basicTeenager', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
-      expect(basicTeenager(13)).toEqual("You are a teenager!");
-    })
+  function basicTeenager (age) => {
+    if (age < 19 && age > 13) {
+      return "You are a teenager!"
 
-    it('should return undefined if the age is not between 13-19', () => {
-      expect(basicTeenager(12)).toBe(undefined)
-    })
-
-  })
+    else {
+      return "You are not a teenager"
+    }
+  }
+}
 
   describe('teenager', () => {
     it('should return "You are a teenager!" if the age is between 13-19', () => {
